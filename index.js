b534bd5a61aa99030a98ddf63246fa10fdc6d46d
Samyak Bhuta <samyak.bhuta@gmail.com>
2016-08-19 16:18:19 +0530

    Adds a server handler for sockets using socket.io and node.js http server

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var PORT = 3000;
+//Handler to give a html page in response
+app.get('/', function(request, response){
+  console.log("Sending html response to client");
+  response.sendFile(__dirname +'/index.html');
+
+});
+
+//assiging a port to node.js server
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	//catching the messages sent by the client sockets
+	socket.on('chat message', function(msg){
+		console.log('Message is : ' + msg);
+	});
+	//executed when the client socket is disconnected
+	socket.on('disconnect', function(){
+		console.log('Disconnected user');
+	});
+
+});
+
+http.listen(3000, function(){
+	console.log('Listening on port 3000');
+});
+console.log('Server is listening on port 3000');
