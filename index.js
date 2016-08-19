5e8f06349ccb6c59b121d01b1ffd7eab4f1388df
shubhamc <samyak.bhuta@gmail.com>
2016-08-19 18:04:52 +0530

    Adds author update check

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/', function(request, response){
 
 });
 
+
 //assiging a port to node.js server
 io.on('connection', function(socket){
 	console.log('a user connected');
@@ -30,3 +31,5 @@ http.listen(3000, function(){
 	console.log('Listening on port 3000');
 });
 console.log('Server is listening on port 3000');
+
+ 58f5c77f0356757b1d6aa79ce6de8b309dea7f30
